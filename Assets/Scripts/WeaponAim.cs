a5ce104e978789c6444dc03b544f881bd5a59a8f
Nick Joebgen <nick.joebgen@gmail.com>
2015-10-01 14:24:13 -0500

    Initial Commit
    Initial Commit for the new Space Scavs project, fixed thanks to Peter
    and Mitch

---
+++ Assets/Scripts/WeaponAim.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponAim : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float AngleRad = Mathf.Atan2 (Input.mousePosition.y - transform.position.y, Input.mousePosition.x - transform.position.x);
+		float AngleDeg = (180 / Mathf.PI) * AngleRad;
+		this.transform.rotation = Quaternion.Euler (0, 0, AngleDeg);
+	}
+}
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weapon : MonoBehaviour {
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.parent != null)
+		{
+			rb.isKinematic = true;
+		}
+	
+	}
+}
